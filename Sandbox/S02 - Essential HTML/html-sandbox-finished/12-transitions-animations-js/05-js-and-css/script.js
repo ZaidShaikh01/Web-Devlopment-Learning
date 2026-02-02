6386d65a0b3ba1375e654ec4495db510d3a2102f
ZaidShaikh <szaid5146@gmail.com>
2026-02-02 09:50:51 +0530

    asdf

--- Sandbox/S02 - Essential HTML/html-sandbox-finished/12-transitions-animations-js/05-js-and-css/script.js
+++
@@ -1,7 +0,0 @@
-const button = document.querySelector('#button');
-const heading = document.querySelector('h1');
-
-button.addEventListener('click', function () {
-  document.body.classList.toggle('dark');
-  heading.classList.toggle('yellow');
-});
