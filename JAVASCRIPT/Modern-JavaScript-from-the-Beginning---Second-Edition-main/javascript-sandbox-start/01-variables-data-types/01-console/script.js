98a659c44be8a429a5b834267417161598c30104
ZaidShaikh <szaid5146@gmail.com>
2026-03-12 12:38:52 +0530

    JavaScript started

---
+++ JAVASCRIPT/Modern-JavaScript-from-the-Beginning---Second-Edition-main/javascript-sandbox-start/01-variables-data-types/01-console/script.js
@@ -0,0 +1,9 @@
+console.log('Hello World');
+console.group('Simpler')
+console.log(100);
+console.error("YO YOO YOOOO");
+console.warn("Heolasdf");
+console.table({name: 'brad', email:'szaid@asdf'});
+console.groupEnd();
+const styles= 'padding:10px; background-color:green; color:red';
+console.log('%cobject',styles);
