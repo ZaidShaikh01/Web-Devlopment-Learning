19b0e8bf3f6cb8ecc820cd1783e05e440b0e9e37
ZaidShaikh <szaid5146@gmail.com>
2026-04-02 18:06:34 +0530

    TYPICODE TODO

---
+++ JAVASCRIPT/Modern-JavaScript-from-the-Beginning---Second-Edition-main/javascript-sandbox-start/10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
@@ -0,0 +1,45 @@
+const apiURL = 'https://jsonplaceholder.typicode.com/todos';
+
+const getTodos = () => {
+  fetch(apiURL + '?_limit=5')
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((todo) => {
+        addTodoToDOM(todo);
+      });
+    });
+};
+const createTodo = (e) => {
+  e.preventDefault();
+  const newTodo = {
+    title: e.target.firstElementChild.value,
+    completed: false,
+  };
+
+  fetch(apiURL, {
+    method: 'POST',
+    body: JSON.stringify(newTodo),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => addTodoToDOM(data));
+};
+
+function addTodoToDOM(todo) {
+  const div = document.createElement('div');
+  div.appendChild(document.createTextNode(todo.title));
+  div.setAttribute('data-id', todo.id);
+
+  if (todo.completed) {
+    div.classList.add('done');
+  }
+  document.getElementById('todo-list').appendChild(div);
+}
+
+const init = () => {
+  document.addEventListener('DOMContentLoaded', getTodos);
+  document.querySelector('#todo-form').addEventListener('submit', createTodo);
+};
+init();
