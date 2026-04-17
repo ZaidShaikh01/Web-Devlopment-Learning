b59fb1b554988115736d45d4f11e0db15c0fa01c
ZaidShaikh <szaid5146@gmail.com>
2026-04-17 12:32:19 +0530

    Object properties

--- JAVASCRIPT/Modern-JavaScript-from-the-Beginning---Second-Edition-main/javascript-sandbox-start/13-oop-constructors-prototypes/04-object-properties/script.js
+++ JAVASCRIPT/Modern-JavaScript-from-the-Beginning---Second-Edition-main/javascript-sandbox-start/13-oop-constructors-prototypes/04-object-properties/script.js
@@ -6,3 +6,33 @@ function Rectangle(name, width, height) {
     return this.width * this.height;
   };
 }
+
+const rect1 = new Rectangle('Recatangle 1', 20, 20);
+const rect2 = new Rectangle('Recatangle 1', 20, 30);
+
+console.log(rect1);
+
+// Adding new functions and parameters
+rect1.color = 'red';
+rect1.perimeter = () => {
+  return (rect1.height + rect1.width) * 2;
+};
+
+// Delete property
+delete rect2.perimeter;
+console.log(rect1.perimeter());
+// check property
+console.log(rect1.hasOwnProperty('color'));
+console.log(rect2.hasOwnProperty('color'));
+// Get keys
+console.log(Object.keys(rect1));
+// get values
+console.log(Object.values(rect1));
+// Get entries
+console.log(Object.entries(rect1));
+
+for (let [key, value] of Object.entries(rect1)) {
+  if (typeof value !== 'function') {
+    console.log(key + ' : ' + value);
+  }
+}
