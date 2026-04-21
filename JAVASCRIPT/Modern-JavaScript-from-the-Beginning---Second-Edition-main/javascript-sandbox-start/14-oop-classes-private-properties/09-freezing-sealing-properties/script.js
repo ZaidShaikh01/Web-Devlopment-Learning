303a50c2be887112c8ae71dc0a0eac22b9c2ba1b
ZaidShaikh <szaid5146@gmail.com>
2026-04-21 08:15:48 +0530

    Freezing & Sealing

--- JAVASCRIPT/Modern-JavaScript-from-the-Beginning---Second-Edition-main/javascript-sandbox-start/14-oop-classes-private-properties/09-freezing-sealing-properties/script.js
+++ JAVASCRIPT/Modern-JavaScript-from-the-Beginning---Second-Edition-main/javascript-sandbox-start/14-oop-classes-private-properties/09-freezing-sealing-properties/script.js
@@ -1,3 +1,29 @@
 // Sealing - Prevents properties from being added or removed. Can still be changed.
 
 // Freezing - Prevents properties from being added, removed or changed
+
+const rectObj = {
+  name: 'Rectangle 1',
+  width: 10,
+  height: 10,
+};
+
+Object.seal(rectObj);
+let descriptors = Object.getOwnPropertyDescriptors(rectObj);
+console.log(descriptors);
+
+rectObj.color = 'red';
+delete rectObj.name;
+rectObj.width = 50;
+console.log(rectObj);
+
+const circleObj = {
+  name: 'Circle 1',
+  radius: 30,
+};
+
+Object.freeze(circleObj);
+circleObj.radius = 50;
+let descriptors1 = Object.getOwnPropertyDescriptors(circleObj);
+console.log(descriptors1);
+console.log(circleObj);
