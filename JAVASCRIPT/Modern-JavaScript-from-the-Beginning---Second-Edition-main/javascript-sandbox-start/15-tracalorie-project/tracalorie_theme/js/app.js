48861d3ea38b95a869237f147b88ebda57bf8e31
ZaidShaikh <szaid5146@gmail.com>
2026-06-10 10:43:31 +0530

    Refractor the method

--- JAVASCRIPT/Modern-JavaScript-from-the-Beginning---Second-Edition-main/javascript-sandbox-start/15-tracalorie-project/tracalorie_theme/js/app.js
+++ JAVASCRIPT/Modern-JavaScript-from-the-Beginning---Second-Edition-main/javascript-sandbox-start/15-tracalorie-project/tracalorie_theme/js/app.js
@@ -113,48 +113,36 @@ class App {
     this._tracker = new CalorieTracker();
     document
       .getElementById('meal-form')
-      .addEventListener('submit', this._newMeal.bind(this));
+      .addEventListener('submit', this._newItem.bind(this, 'meal'));
     document
       .getElementById('workout-form')
-      .addEventListener('submit', this._newWorkout.bind(this));
+      .addEventListener('submit', this._newItem.bind(this, 'workout'));
   }
-  _newMeal(e) {
+  _newItem(type, e) {
     e.preventDefault();
-    const name = document.getElementById('meal-name');
-    const calories = document.getElementById('meal-calories');
+
+    const name = document.getElementById(`${type}-name`);
+    const calories = document.getElementById(`${type}-calories`);
 
     // Validate inputs
     if (name.value === '' || calories.value === '') {
       alert('Please fill in all fields');
       return;
     }
-    const meal = new Meal(name.value, +calories.value); // Without + it concats string
 
-    this._tracker.addMeal(meal);
+    if (type === 'meal') {
+      const meal = new Meal(name.value, +calories.value); // Without + it concats string
+
+      this._tracker.addMeal(meal);
+    } else {
+      const workout = new Workout(name.value, +calories.value);
+      this._tracker.addWorkout(meal);
+    }
     name.value = '';
     calories.value = '';
 
-    const collapseMeal = document.getElementById('collapse-meal');
+    const collapseMeal = document.getElementById(`collapse-${type}`);
     const bsCollapse = new bootstrap.Collapse(collapseMeal, { toggle: true });
-  }
-
-  _newWorkout(e) {
-    e.preventDefault();
-    const name = document.getElementById('workout-name');
-    const calories = document.getElementById('workout-calories');
-
-    // Validate inputs
-    if (name.value === '' || calories.value === '') {
-      alert('Please fill in all fields');
-      return;
-    }
-    const workout = new Workout(name.value, +calories.value); // Without + it concats string
-
-    this._tracker.addWorkout(workout);
-    name.value = '';
-    calories.value = '';
-    const collapseWorkout = document.getElementById('collapse-workout');
-    const bsCollapse = new bootstrap.Collapse(collapseWorkout, { toggle: true });
   }
 }
 
