751ec223b8fe6cdbfc9478098c17db5a6c5f86a6
ZaidShaikh <szaid5146@gmail.com>
2026-06-12 11:57:30 +0530

    Persist total calories

--- JAVASCRIPT/Modern-JavaScript-from-the-Beginning---Second-Edition-main/javascript-sandbox-start/15-tracalorie-project/tracalorie_theme/js/app.js
+++ JAVASCRIPT/Modern-JavaScript-from-the-Beginning---Second-Edition-main/javascript-sandbox-start/15-tracalorie-project/tracalorie_theme/js/app.js
@@ -1,7 +1,7 @@
 class CalorieTracker {
   constructor() {
     this._calorieLimit = Storage.getCalorieLimit();
-    this._totalCalories = 0;
+    this._totalCalories = Storage.getTotalCalories(0);
     this._meals = [];
     this._workouts = [];
     this._displayCaloriesTotal();
@@ -16,12 +16,15 @@ class CalorieTracker {
   addMeal(meal) {
     this._meals.push(meal);
     this._totalCalories += meal.calories;
+    Storage.updateTotalCalories(this._totalCalories);
     this._displayNewMeal(meal);
     this._render();
   }
   addWorkout(workout) {
     this._workouts.push(workout);
     this._totalCalories -= workout.calories;
+    Storage.updateTotalCalories(this._totalCalories);
+
     this._displayNewWorkout(workout);
     this._render();
   }
@@ -31,6 +34,8 @@ class CalorieTracker {
     if (index !== -1) {
       const meal = this._meals[index];
       this._totalCalories -= meal.calories;
+      Storage.updateTotalCalories(this._totalCalories);
+
       this._meals.splice(index, 1);
       this._render();
     }
@@ -40,6 +45,8 @@ class CalorieTracker {
     if (index !== -1) {
       const workout = this._workouts[index];
       this._totalCalories += workout.calories;
+      Storage.updateTotalCalories(this._totalCalories);
+
       this._workouts.splice(index, 1);
       this._render();
     }
@@ -205,6 +212,20 @@ class Storage {
 
   static setCalorieLimit(calorieLimit) {
     localStorage.setItem('calorieLimit', calorieLimit);
+  }
+
+  static getTotalCalories(defaultCalories = 0) {
+    let totalCalories;
+    if (localStorage.getItem('totalCalories') === null) {
+      totalCalories = defaultCalories;
+    } else {
+      totalCalories = +localStorage.getItem('totalCalories');
+    }
+    return totalCalories;
+  }
+
+  static updateTotalCalories(calories) {
+    localStorage.setItem('totalCalories', calories);
   }
 }
 class App {
