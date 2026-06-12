810f08c7d544003ca267ca0cad879850af272fa7
ZaidShaikh <szaid5146@gmail.com>
2026-06-12 12:19:35 +0530

    Remove meals/workout

--- JAVASCRIPT/Modern-JavaScript-from-the-Beginning---Second-Edition-main/javascript-sandbox-start/15-tracalorie-project/tracalorie_theme/js/app.js
+++ JAVASCRIPT/Modern-JavaScript-from-the-Beginning---Second-Edition-main/javascript-sandbox-start/15-tracalorie-project/tracalorie_theme/js/app.js
@@ -36,8 +36,8 @@ class CalorieTracker {
       const meal = this._meals[index];
       this._totalCalories -= meal.calories;
       Storage.updateTotalCalories(this._totalCalories);
-
       this._meals.splice(index, 1);
+      Storage.removeMeal(id);
       this._render();
     }
   }
@@ -47,8 +47,8 @@ class CalorieTracker {
       const workout = this._workouts[index];
       this._totalCalories += workout.calories;
       Storage.updateTotalCalories(this._totalCalories);
-
       this._workouts.splice(index, 1);
+      Storage.removeWorkout(id);
       this._render();
     }
   }
@@ -252,6 +252,17 @@ class Storage {
     meals.push(meal);
     localStorage.setItem('meals', JSON.stringify(meals));
   }
+
+  static removeMeal(id) {
+    const meals = Storage.getMeals();
+    meals.forEach((meal, index) => {
+      if (meal.id === id) {
+        meals.splice(index, 1);
+      }
+    });
+    localStorage.setItem('meals', JSON.stringify(meals));
+  }
+
   static getWorkouts() {
     let workouts;
     if (localStorage.getItem('workouts') === null) {
@@ -264,6 +275,15 @@ class Storage {
   static saveWorkouts(workout) {
     const workouts = Storage.getWorkouts();
     workouts.push(workout);
+    localStorage.setItem('workouts', JSON.stringify(workouts));
+  }
+  static removeWorkout(id) {
+    const workouts = Storage.getWorkouts();
+    workouts.forEach((workout, index) => {
+      if (workout.id === id) {
+        workouts.splice(index, 1);
+      }
+    });
     localStorage.setItem('workouts', JSON.stringify(workouts));
   }
 }
