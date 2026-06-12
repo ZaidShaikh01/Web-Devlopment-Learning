a294977f93c169de0c7ed6dfe3af51cededeaf49
ZaidShaikh <szaid5146@gmail.com>
2026-06-12 12:24:31 +0530

    Clear Storage Items

--- JAVASCRIPT/Modern-JavaScript-from-the-Beginning---Second-Edition-main/javascript-sandbox-start/15-tracalorie-project/tracalorie_theme/js/app.js
+++ JAVASCRIPT/Modern-JavaScript-from-the-Beginning---Second-Edition-main/javascript-sandbox-start/15-tracalorie-project/tracalorie_theme/js/app.js
@@ -10,6 +10,8 @@ class CalorieTracker {
     this._displayCaloriesBurned();
     this._displayCaloriesRemaining();
     this._displayCaloriesProgress();
+
+    document.getElementById('limit').value = this._calorieLimit;
   }
 
   //   Public Methods //
@@ -57,6 +59,7 @@ class CalorieTracker {
     this._totalCalories = 0;
     this._meals = [];
     this._workouts = [];
+    Storage.clearAll();
     this._render();
   }
 
@@ -285,6 +288,12 @@ class Storage {
       }
     });
     localStorage.setItem('workouts', JSON.stringify(workouts));
+  }
+  static clearAll() {
+    localStorage.removeItem('totalCalories');
+    localStorage.removeItem('meals');
+    localStorage.removeItem('workouts');
+    // localStorage.clear();
   }
 }
 class App {
