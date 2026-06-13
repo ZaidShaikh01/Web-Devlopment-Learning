b527bab9ff5841a8c8e3891b68ccb28d556eee70
ZaidShaikh <szaid5146@gmail.com>
2026-06-13 14:31:46 +0530

    NPM packages

--- JAVASCRIPT/Modern-JavaScript-from-the-Beginning---Second-Edition-main/javascript-sandbox-start/16-modules-and-tooling/Node-playground/app.js
+++ JAVASCRIPT/Modern-JavaScript-from-the-Beginning---Second-Edition-main/javascript-sandbox-start/16-modules-and-tooling/Node-playground/app.js
@@ -1,10 +1,7 @@
-const { capitalizeWords, makeMoney } = require('./utils');
+const axios = require('axios');
+async function getPost() {
+  const res = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
+  console.log(res.data);
+}
 
-const Person = require('./Person');
-
-console.log(makeMoney(400));
-console.log(capitalizeWords('lsdkfja'));
-
-const person1 = new Person('John', 40);
-
-person1.greet();
+getPost();
